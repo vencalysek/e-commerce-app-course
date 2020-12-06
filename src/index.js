f1bc9b6d158fc8c0a752b7d91ee258a8092883d3
Venca Lysek <vencalysek@seznam.cz>
2020-12-06 21:55:23 +0100

    add redux-persist lib for cart storage into local

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 
 import {Provider} from "react-redux";
-import store from './redux/store'
+import {store, persistor} from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 import "./index.css";
 
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
