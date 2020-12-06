821d7676f8f3305943d2e8063d5212d230cc30d3
Venca Lysek <vencalysek@seznam.cz>
2020-12-06 19:12:02 +0100

    added addItem and removeItem func to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import {CartActionTypes} from "./cart.types";
-import { addItemToCart } from './cart.utils'
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -8,7 +8,6 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-
     case CartActionTypes.TOOGLE_CART_HIDDEN:
       return {
         ...state,
@@ -21,9 +20,25 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
 
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id),
+      };
+
     default:
       return state;
   }
 };
 
 export default cartReducer;
+
+// state.cartItems.map(cartItem => {
+//   cartItem.id !== action.payload.id ? cartItem.quantity++ : cartItem
+// })
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,19 +1,33 @@
 // function will check items in cart and insted of puting duplicates, will update item quantity
 
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-
   // check if item eqxist
   const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
   if (existingCartItem) {
-    return cartItems.map(cartItem => 
+    // if item already exist, add +1 to quantity
+    return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
-        ? {...cartItem, quantity: cartItem.quantity + 1} 
-        : cartItem)
+        ? {...cartItem, quantity: cartItem.quantity + 1}
+        : cartItem
+    );
   }
+  // if item doesn't exist create new item in cart
 
-  return [...cartItems, { ...cartItemToAdd, quantity: 1}]
-
+  return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
 
 // explanation to duplicite check id logic: .map is used to find "right" item on which to use quantity +1
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // look or needed item inside cartItems store and decrease quantity by 1
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1} : cartItem
+  );
+};
